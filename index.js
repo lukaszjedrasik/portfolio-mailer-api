c4cd5e0e26867cc741e61bf1a9ee60984f06dac8
Łukasz Jędrasik <40273258+lukaszjedrasik@users.noreply.github.com>
2019-05-07 21:54:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const port = 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Welcome Page
+app.get('/', (req, res) => {
+    res.send('Welcome to the SendGrid Email Server | Łukasz Jędrasik')
+});
+
+// Email Page
+app.post('/send', (req, res) => {
+    const msg = {
+        to: 'lukasz.jedrasik@icloud.com',
+        from: req.body.from,
+        subject: req.body.subject,
+        text: req.body.text
+    };
+    sgMail.send(msg)
+});
+
+app.listen(port, () => console.log(`running on port ${port}`));
