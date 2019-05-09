a600083aca20f494f31b6cd503c565b34f354f77
Łukasz Jędrasik <40273258+lukaszjedrasik@users.noreply.github.com>
2019-05-09 18:28:17 +0200

    update

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY || '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');
-const port = 8080;
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const port = 8181;
 
 app.use(cors());
 app.use(bodyParser.json());
